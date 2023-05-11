8688dae4605f68209ed2f32cd61d0b42352d6d09
Paola Mangelli <mangelli.paola90@gmail.com>
2023-05-11 11:46:03 -0400

    clock icon

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-// import fontAwesome from "@fortawesome/fontawesome";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
 function SimpleCounter(props){
     return (
-        <div className="all">
-            <div className="clock"></div>
-            <div className="six">{props.digitSix %10}</div>
-            <div className="five">{props.digitFive %10}</div>
-            <div className="four">{props.digitFour %10}</div>
-            <div className="three">{props.digitThree %10}</div>
-            <div className="two">{props.digitTwo %10}</div>
-            <div className="one">{props.digitOne %10}</div>
-        </div>
+        <>
+            <div className="all">
+                <div className="clock"><i className="far fa-clock"></i></div>
+                <div className="six">{props.digitSix %10}</div>
+                <div className="five">{props.digitFive %10}</div>
+                <div className="four">{props.digitFour %10}</div>
+                <div className="three">{props.digitThree %10}</div>
+                <div className="two">{props.digitTwo %10}</div>
+                <div className="one">{props.digitOne %10}</div>
+            </div>
+        </>
     )
 }
 SimpleCounter.propTypes ={
